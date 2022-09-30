241612ed195368c3898105741cc1dbd33945816b
nguyenminhthoa1002 <minhthoa100299@gmail.com>
2022-09-30 09:37:48 +0700

    lan-1

---
+++ src/rikkeiAcademy/Student.java
@@ -0,0 +1,12 @@
+package rikkeiAcademy;
+
+public class Student {
+    private int studentID;
+    private String name;
+    private static String university = "University of Languages & International Studies";
+
+    public Student() {
+        studentID = 18041298;
+        name = "Nguyen Minh Thoa";
+    }
+}
